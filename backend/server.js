c019144f14aa88b245a509b77d1d5df7042d812d
AAYUSH KUMAR <aayushkumarirl@gmail.com>
2021-06-25 21:16:40 +0530

    Connected to mongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import colors from "colors";
+
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is up and running");
@@ -24,6 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
