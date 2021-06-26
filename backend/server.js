735f819d232f5067e76daee5ba189960ebeb4dec
AAYUSH KUMAR <aayushkumarirl@gmail.com>
2021-06-26 13:49:29 +0530

    Added Error Handling

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -12,6 +13,12 @@ const app = express();
 connectDB();
 
 app.use("/api/products", productRoutes);
+
+// Error handling for 404 responses
+app.use(notFound);
+
+// Overwriting default express error
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
