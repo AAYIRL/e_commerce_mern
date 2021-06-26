07a60d1027123b28eb813b54158d1917a917d1b2
AAYUSH KUMAR <aayushkumarirl@gmail.com>
2021-06-26 16:14:44 +0530

    Added Store for Redux

---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+// Place where we connect all the reducers
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers();
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
